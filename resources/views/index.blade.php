79cb7174a5e714c526eab854946ed6849d23e97e
valeryan1 <114754068+valeryan1@users.noreply.github.com>
2024-04-28 20:36:20 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with porto landing page.">
+    <meta name="author" content="alvin">
+    <title>Alvin's landing page | Portofolio Website </title>
+    <!-- font icons -->
+    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+    <!-- Bootstrap + porto main styles -->
+	<link rel="stylesheet" href="assets/css/porto.css">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+
+    <!-- Page navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" data-spy="affix" data-offset-top="0">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="assets/imgs/tes2.svg" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto align-items-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#service">Skills</a>
+                    </li>                   
+                    <li class="nav-item">
+                        <a class="nav-link" href="#blog">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="- btn btn-dark rounded ml-4 nav-link2" href="#contact">Contact me</a>
+                    </li>
+                </ul>
+            </div>
+        </div>          
+    </nav>
+    <!-- End of page navibation -->
+
+    <!-- Page Header -->
+    <header class="header" id="home">
+        <div class="container">
+            <div class="infos">
+                <h6 class="subtitle">hello,I'm</h6>
+                <h6 class="title">Alvin Ariyanto P</h6>
+                <p>Full Stack Engineer</p>
+
+                <div class="buttons pt-3">
+                    <button class="btn btn-primary rounded">Contact me</button>
+                    {{-- <button class="btn btn-dark rounded">DOWNLOAD CV</button> --}}
+                </div>      
+
+                <div class="socials mt-4">
+                    <a class="social-item" href="javascript:void(0)"><i class="ti-instagram"></i></a>
+                    <a class="social-item" href="javascript:void(0)"><i class="ti-linkedin"></i></a>
+                    <a class="social-item" href="javascript:void(0)"><i class="ti-github"></i></a>
+                </div>
+            </div>              
+            <div class="img-holder">
+                <img src="assets/imgs/foto-2.png" alt="">
+            </div>      
+        </div>  
+    </header>
+    <!-- End of Page Header -->
+    
+    <!-- About section -->
+    <section id="about" class="section mt-3">
+        <div class="container mt-5">
+            <div class="row text-center text-md-left">
+                <div class="col-md-3">
+                    <img src="assets/imgs/foto.jpg" alt="" class="img-thumbnail mb-4">
+                </div>
+                <div class="pl-md-4 col-md-9">
+                    <h6 class="title">Alvin Ariyanto Putra</h6>
+                    <p class="subtitle"> Full Stack Engineer</p>
+                    <p>Hi I'm a Computer Science student from Batam, Indonesia, currently at Binus University, interesting in mobile and web development. As a Tech Enthusiast, I'm dedicated to creating user-friendly solutions and staying current with industry trends. I'm committed to lifelong learning and eager to connect with like-minded individuals passionate about technology and innovation.</p>
+                    <button class="btn btn-primary rounded mt-3">Contact me</button>                   
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Service section -->
+    <section id="service" class="section">
+        <div class="container text-center">
+            <h6 class="subtitle">Skills</h6>
+            <h6 class="section-title mb-4">What I Do</h6>
+
+            <div class="row">
+                <div class="col-sm-6 col-md-3 mb-4">
+                    <div class="custom-card card border">
+                        <div class="card-body">
+                            <i class="icon ti-html5"></i>
+                            <h5>HTML</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4">
+                    <div class="custom-card card border">
+                        <div class="card-body">
+                            <i class="icon ti-desktop"></i>
+                            <h5>Web Programming</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4">
+                    <div class="custom-card card border">
+                        <div class="card-body">
+                            <i class="icon ti-crown"></i>
+                            <h5>UI/UX Design</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4">
+                    <div class="custom-card card border">
+                        <div class="card-body">
+                            <i class="icon ti-mobile"></i>
+                            <h5>Mobile Programming</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End of Sectoin -->
+
+    <!-- Skills section -->
+    <section class="section">
+        <div class="container text-center">
+            <div class="row text-left">
+                <div class="col-sm-6">
+                    <h6 class="mb-3">C++</h6>
+                    <div class="progress">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 89%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span>89%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <h6 class="mb-3">UI/UX Design</h6>
+                    <div class="progress">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 83%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span>83%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <h6 class="mb-3">HTML</h6>
+                    <div class="progress">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 95%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span>95%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <h6 class="mb-3">Java</h6>
+                    <div class="progress">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span>90%</span></div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </section>
+    <!-- End of Skills sections -->
+
+
+
+   
+
+    <!-- Portofolio Section -->
+    <section id="blog" class="section">
+        <div class="container text-center">
+            <h6 class="subtitle">Portofolio</h6>
+            <h6 class="section-title mb-4">Check my Portofolio</h6>
+            <p class="mb-5 pb-4">Explore my portfolio to see my latest projects! From web design to app development, each piece reflects my passion for creativity and innovation. Get in touch for collaborations or inquiries.</p>
+
+            <div class="row text-left">
+                <div class="col-md-4 mx-auto">
+                    <div class="card border mb-4">
+                        <img src="assets/imgs/AR.png" alt="" class="card-img-top w-100">
+                        <div class="card-body">
+                            <h5 class="card-title">Augmented Reality for Real Estate Application</h5>
+                            <p>We designed an augmented reality application specifically for real estate, incorporating marker-based technology. Through this app, users can explore virtual property renderings overlaid onto their real-world environment by scanning markers. This innovative approach enhances the traditional real estate experience, providing immersive property tours for potential buyers, thus showcasing our commitment to leveraging cutting-edge technology in the real estate industry.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mx-auto">
+                    <div class="card border mb-4">
+                        <img src="assets/imgs/gwk.png" alt="" class="card-img-top w-60">
+                        <div class="card-body">
+                            <h5 class="card-title">Web Layouts</h5>
+                            <p>"Indosight is an Augmented Reality (AR) initiative aimed at showcasing Indonesian cultural heritage. Using marker-based cards, users can scan images to unlock 3D representations and detailed information about each cultural artifact. Our project aims to deepen appreciation and understanding of Indonesian culture through immersive, educational experiences.</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- Hire me section -->
+    <section class="bg-gray p-0 section">
+        <div class="container">
+            <div class="card bg-primary">
+                <div class="card-body text-light">
+                    <div class="row align-items-center">
+                        <div class="col-sm-9 text-center text-sm-left">
+                            <h5 class="mt-3">Reach out me!</h5>
+                            <p class="mb-3">Have a question in mind? Don't hesitate to reach out!</p>
+                        </div>
+                        <div class="col-sm-3 text-center text-sm-right ">
+                            <a class="btn btn-light rounded nav-link2" href="#contact">Contact</a>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </section>      
+    <!-- End od Hire me section. -->
+
+    <!-- Contact Section -->
+    <section id="contact" class="position-relative section">
+        <div class="container text-center">
+            <h6 class="subtitle">Contact</h6>
+            <h6 class="section-title mb-4">Get In Touch With Me</h6>
+
+            <div class="contact text-left">
+                <div class="form">
+                    <h6 class="subtitle">Available 24/7</h6>
+                    <h6 class="section-title mb-4">Get In Touch</h6>
+                    <form>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="Subject" class="form-control" id="exampleInputPassword1" placeholder="Subject" required>
+                        </div>
+                        <div class="form-group">
+                            <textarea name="contact-message" id="" cols="30" rows="5" class="form-control" placeholder="Message"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block rounded w-lg">Send Message</button>
+                    </form>
+                </div>
+                <div class="contact-infos">
+                    <div class="item">
+                        <i class="ti-location-pin"></i>
+                        <div class="">
+                            <h5>Location</h5>
+                            <p> Malang, Jawa Timur, Indonesia</p>
+                        </div>                          
+                    </div>
+                    <div class="item">
+                        <i class="ti-mobile"></i>
+                        <div>
+                            <h5>Phone Number</h5>
+                            <p>(+62) 82321700064</p>
+                        </div>                          
+                    </div>
+                    <div class="item">
+                        <i class="ti-email"></i>
+                        <div class="mb-0">
+                            <h5>Email Address</h5>
+                            <p>alvin.putra004@binus.ac.id</p>
+                        </div>
+                    </div>
+                </div>                  
+            </div>
+        </div>  
+           
+    </section>
+    <!-- End of Contact Section -->
+
+    <!-- Page Footer -->
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-sm-6">
+                    <p>Copyright <script>document.write(new Date().getFullYear())</script> &copy; <a href="" target="_blank">Alvin</a></p>
+                </div>
+                <div class="col-sm-6">
+                    <div class="socials">
+                        <a class="social-item" href="javascript:void(0)"><i class="ti-instagram"></i></a>
+                        <a class="social-item" href="javascript:void(0)"><i class="ti-google"></i></a>
+                        <a class="social-item" href="javascript:void(0)"><i class="ti-github"></i></a>
+                        <a class="social-item" href="javascript:void(0)"><i class="ti-twitter"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer> 
+    <!-- End of page footer -->
+	
+	<!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+    <!-- bootstrap 3 affix -->
+	<script src="assets/vendors/bootstrap/bootstrap.affix.js"></script>
+
+    <!-- porto js -->
+    <script src="assets/js/porto.js"></script>
+
+</body>
+</html>
